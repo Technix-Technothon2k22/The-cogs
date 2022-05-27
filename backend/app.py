ef7c506f2b32bdaabca4d0ca76a1f461bf3fb009
D-coder22 <amitnmf@gmail.com>
2022-05-27 17:32:14 +0530

    Delete node API done

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
-from crypt import methods
-from sqlite3 import Cursor
-from typing import Collection
 from flask import Flask, request, jsonify, json
 from pymongo import MongoClient
 from bson import json_util
+from bson.objectid import ObjectId
 
 
 client = MongoClient(
@@ -16,12 +14,6 @@ cursor = collection.find()
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
-def home():
-    if request.method == "GET":
-        return "<h1>HI</h1>"
-
-
 @app.route("/get-data", methods=["GET"])
 def getData():
     if request.method == "GET":
@@ -31,6 +23,24 @@ def getData():
 
         data = json.loads(json_util.dumps(data))
         return jsonify({"result": data}), 200
+
+# @app.route("/add-node", methods=["PUT"])
+# def addNode():
+#   if request.method == "PUT":
+
+
+@app.route("/del-node/<id>", methods=["DELETE"])
+def delNode(id):
+    if request.method == "DELETE":
+        try:
+            if collection.find_one({"_id": ObjectId(id)}) is not None:
+                collection.delete_one({"_id": ObjectId(id)})
+            else:
+                raise Exception("No object")
+        except(Exception):
+            return jsonify("Error"), 503
+
+        return jsonify("Deleted"), 200
 
 
 if __name__ == "__main__":
