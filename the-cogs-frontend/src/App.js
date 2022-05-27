58aa638ffe60eb60ef207f3c9cc0a847031ec196
Nishant Kedar <nkedar@outlook.com>
2022-05-27 16:08:33 +0530

    create component for map

--- the-cogs-frontend/src/App.js
+++ the-cogs-frontend/src/App.js
@@ -1,6 +1,6 @@
+import Map from "./MapCont";
 import logo from "./logo.svg";
 import "./App.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import PrimarySearchAppBar from "./Appbar";
 
@@ -8,12 +8,7 @@ function App() {
   return (
     <div className="App">
       <PrimarySearchAppBar />
-      <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
+      <Map />
     </div>
   );
 }
