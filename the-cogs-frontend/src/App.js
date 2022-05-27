6ba139fb6561acf60d3e8c8ffbbb3a7f495d8c9c
Nishant Kedar <nkedar@outlook.com>
2022-05-27 19:24:21 +0530

    working node markers

--- the-cogs-frontend/src/App.js
+++ the-cogs-frontend/src/App.js
@@ -1,17 +1,55 @@
+import React from "react";
 import Map from "./MapCont";
 import logo from "./logo.svg";
 import "./App.css";
 
 import PrimarySearchAppBar from "./Appbar";
 import PermanentDrawerRight from "./sidebar";
+import { Box } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <PrimarySearchAppBar />
-      <Map />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+      dataIsLoaded: false,
+    };
+  }
+  componentDidMount() {
+    fetch("http://192.168.0.121:5001/get-data")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState(
+          {
+            nodes: json,
+            dataIsLoaded: true,
+          },
+          () => console.log("data loaded")
+        );
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Box sx={{ flexGrow: 1 }}>
+          <PrimarySearchAppBar {...this.state.nodes} />
+          {this.state.dataIsLoaded ? (
+            <Map {...this.state.nodes} />
+          ) : (
+            <h1> Loading </h1>
+          )}
+
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            {this.state.dataIsLoaded ? (
+              <PermanentDrawerRight />
+            ) : (
+              <h1> Loading </h1>
+            )}
+          </Box>
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default App;
--- the-cogs-frontend/src/MapCont.js
+++ the-cogs-frontend/src/MapCont.js
@@ -1,36 +1,8 @@
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
-const Data = {
-  parks: [
-    {
-      type: "Feature",
-      properties: {
-        PARK_ID: 960,
-        NAME: "Bearbrook Skateboard Park",
-        DESCRIPTIO: "Flat asphalt surface, 5 components",
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [15.283, 73.98],
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        PARK_ID: 1219,
-        NAME: "Bob MacQuarrie Skateboard Park (SK8 Extreme Park)",
-        DESCRIPTIO:
-          "Flat asphalt surface, 10 components, City run learn to skateboard programs, City run skateboard camps in summer",
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [15.3982, 73.8113],
-      },
-    },
-  ],
-};
-const MapCont = () => {
+const MapCont = (props) => {
+  console.log(props);
   const [activePark, setActivePark] = useState(null);
   return (
     <MapContainer center={[15.283, 73.98]} zoom={12} scrollWheelZoom={false}>
@@ -38,18 +10,21 @@ const MapCont = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {Data.parks.map((park) => (
-        <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[0],
-            park.geometry.coordinates[1],
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
-        />
-      ))}
+      {props.result.map((node) => {
+        console.log(node.location);
+        return (
+          <Marker
+            key={node.$oid}
+            position={[
+              node.location.lat.$numberDecimal,
+              node.location.long.$numberDecimal,
+            ]}
+            onClick={() => {
+              setActivePark(node);
+            }}
+          />
+        );
+      })}
     </MapContainer>
   );
 };
