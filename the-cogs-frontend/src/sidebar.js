1795d9fff7af32c2bf551be3b42225fa1dd69132
D-coder22 <amitnmf@gmail.com>
2022-05-27 23:24:12 +0530

    Accordion Dropdown added

--- the-cogs-frontend/src/sidebar.js
+++ the-cogs-frontend/src/sidebar.js
@@ -1,8 +1,5 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
@@ -13,6 +10,11 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const drawerWidth = "30%";
 
@@ -35,7 +37,24 @@ export default function PermanentDrawerRight(props) {
           .map((text, index) => (
             <ListItem key={text.$oid} disablePadding>
               <ListItemButton>
-                <ListItemText primary={text.status} />
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography color="red">{text.status}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography color="red">
+                      Voltage = {text.volt}V<br />
+                      Phase = {text.phase}
+                      <br />
+                      Co-ordinates = {text.location.lat} N, {text.location.long}{" "}
+                      E
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
               </ListItemButton>
             </ListItem>
           ))}
