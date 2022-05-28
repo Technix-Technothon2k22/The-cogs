74bbc20db36aab6bc024bde85765581876f8eb93
Nishant Kedar <nkedar@outlook.com>
2022-05-28 08:02:52 +0530

    node sidebar

--- the-cogs-frontend/src/sidebar.js
+++ the-cogs-frontend/src/sidebar.js
@@ -1,23 +1,17 @@
 import * as React from "react";
 import Drawer from "@mui/material/Drawer";
-import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Grid from '@mui/material/Grid'
+import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
 
-const drawerWidth = "30%";
+const drawerWidth = "2  0%";
 
 const selectColor = (status) => {
   if (status === "online") return "green";
@@ -36,14 +30,16 @@ export default function PermanentDrawerRight(props) {
       }}
       anchor={"right"}
     >
-      <Toolbar />
-      <Divider />
       <List>
         {props.result
           .filter((item) => item.status === "offline")
-          .map((text, index) => (
-            <ListItem key={text.$oid} disablePadding>
-              <ListItemButton>
+          .map((node, index) => {
+            const changeStatus = () =>
+              fetch(`http://localhost:5001/change-status/${node._id.$oid}`, {
+                method: "PUT",
+              });
+            return (
+              <ListItem key={node._id.$oid} disablePadding>
                 <Accordion>
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
@@ -51,44 +47,68 @@ export default function PermanentDrawerRight(props) {
                     id="panel1a-header"
                   >
                     <Grid container>
-                    <Grid item xs={11}>
-                    <Typography color={selectColor(text.status)}>
-                      {text.status}
-                    </Typography>
-                    </Grid>
-                    <Grid item xs={1}>
-                    </Grid>
+                      <Grid item xs={11}>
+                        <Typography color={selectColor(node.status)}>
+                          {node.status}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={1}></Grid>
                     </Grid>
                   </AccordionSummary>
                   <AccordionDetails>
-                    <Typography color={selectColor(text.status)}>
-                      Voltage = {text.volt}V<br />
-                      Phase = {text.phase}
+                    <Typography color={selectColor(node.status)}>
+                      Voltage = {node.volt}V<br />
+                      Phase = {node.phase}
                       <br />
-                      Co-ordinates = {text.location.lat} N, {text.location.long}{" "}
+                      Co-ordinates = {node.location.lat} N, {node.location.long}{" "}
                       E
-                      <br/>
-                      <Button variant="contained" color="warning">Repair</Button>
+                      <br />
+                      <Button
+                        variant="contained"
+                        color="warning"
+                        onClick={changeStatus}
+                      >
+                        Repair
+                      </Button>
                     </Typography>
                   </AccordionDetails>
                 </Accordion>
-              </ListItemButton>
-            </ListItem>
-          ))}
+              </ListItem>
+            );
+          })}
+      </List>
+      <List>
+        {props.result
+          .filter((item) => item.status === "online")
+          .map((node, index) => {
+            return (
+              <ListItem key={node._id.$oid} disablePadding>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography color={selectColor(node.status)}>
+                      {node.locality}
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography color={selectColor(node.status)}>
+                      Voltage = {node.volt}V<br />
+                      Phase = {node.phase}
+                      <br />
+                      Co-ordinates = {node.location.lat} N, {node.location.long}{" "}
+                      E
+                      <br />
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </ListItem>
+            );
+          })}
       </List>
       <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
     </Drawer>
   );
 }
